94b6569c66657b18608020e3ebc0e030653cdfe4
Prajakta Patil <nprajakta2724@gmail.com>
2026-05-31 14:46:01 +0530

    feat: Setup IAM roles foe Jenkins & TF

---
+++ netstream-platform/terraform/environments/dev/main.tf
@@ -0,0 +1,9 @@
+module "iam" {
+  source                  = "../../modules/iam"
+  project_name            = var.project_name
+  environment             = var.environment
+  aws_region              = var.aws_region
+  account_id              = data.aws_caller_identity.current.account_id
+  sso_user_arn            = data.aws_caller_identity.current.arn
+  permission_boundary_arn = var.permission_boundary_arn
+}
---
+++ netstream-platform/terraform/modules/iam/main.tf
@@ -0,0 +1,59 @@
+# This only needs to be written ONCE in the entire folder
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+# JenkinsMasterRole
+resource "aws_iam_role" "jenkins_master_role" {
+  name = "${var.project_name}-JenkinsMasterRole"
+  # Trust Policy allowing EC2 instances to wear this identity
+  assume_role_policy = jsonencode(local.jenkins_master_config)
+}
+# 1.Attach the Operational Permissions
+resource "aws_iam_role_policy" "jenkins_master_policy" {
+  name   = "${var.project_name}-JenkinsMasterPolicy"
+  role   = aws_iam_role.jenkins_master_role.id
+  policy = jsonencode(local.jenkins_master_config)
+}
+#2. Create the EC2 Instance Profile Container
+resource "aws_iam_instance_profile" "jenkins_master_instance_profile" {
+  name = "${var.project_name}-JenkinsMasterInstanceProfile"
+  role = aws_iam_role.jenkins_master_role.name
+}
+
+# JenkinsAgentRole
+resource "aws_iam_role" "jenkins_agent_role" {
+  name               = "${var.project_name}-JenkinsAgentRole"
+  assume_role_policy = jsonencode(local.jenkins_agent_config)
+
+}
+resource "aws_iam_role_policy" "jenkins_agent_policy" {
+  name   = "${var.project_name}-JenkinsAgentPolicy"
+  role   = aws_iam_role.jenkins_agent_role.id
+  policy = jsonencode(local.jenkins_agent_config)
+}
+#1. Allow Jenkins Agent to assume the Jenkins Master Role
+resource "aws_iam_role_policy" "jenkins_agent_assume_master_policy" {
+  name   = "${var.project_name}-JenkinsAgentAssumeMasterPolicy"
+  role   = aws_iam_role.jenkins_agent_role.id
+  policy = locals.jenkins_master_config
+}
+
+#2. Create the EC2 Instance Profile Container
+resource "aws_iam_instance_profile" "jenkins_agent_instance_profile" {
+  name = "${var.project_name}-JenkinsAgentInstanceProfile"
+  role = aws_iam_role.jenkins_agent_role.name
+}
+
+#TerraformExecutionRole
+resource "aws_iam_role" "terraform_execution_role" {
+  name               = "${var.project_name}-TerraformExecutionRole"
+  assume_role_policy = jsonencode(local.tf_exec_role_config.trust_policy)
+}
+resource "aws_iam_role_policy" "terraform_execution_policy" {
+  name   = "${var.project_name}-TerraformExecutionPolicy"
+  role   = aws_iam_role.terraform_execution_role.id
+  policy = jsonencode(local.tf_exec_role_config.operational_policy)
+}
+
+
+
